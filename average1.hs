c336e09ed25ad60ded1783c1aed886c721d8da42
lucasherlon <lucasherlondsmc@gmail.com>
2022-12-25 10:47:49 -0300

    exercicio 1005

---
+++ average1.hs
@@ -0,0 +1,11 @@
+import Text.Printf
+
+main :: IO()
+main = do
+      a' <- getLine
+      b' <- getLine
+      let a = read a' :: Double
+      let b = read b' :: Double
+      let average = ((a * 3.5) + (b * 7.5)) / 11
+
+      putStrLn $ printf "%.5f" average
