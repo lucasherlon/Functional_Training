f48d5eca742131d61726c5cf767ba26fc0693943
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2023-03-31 20:48:25 -0300

    Fsharp palindrome detector

---
+++ PalindromeDetector/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let isPalindrome (word : string) =
+    word = word[0..]
+
+printfn "Type a word to find out if it's a PALINDROME"
+let word = Console.ReadLine()
+let result = isPalindrome(word)
+printfn $"{result}"
+
+
+
+
+
+
