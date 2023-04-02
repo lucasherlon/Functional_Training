4b3c21fa424b444166771a67e4fa8d7e59767b62
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2023-04-01 21:17:49 -0300

    F# codewars you're a square

---
+++ YouSquare/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let isSquare x: bool =
+    if x < 0 then
+        false
+    else
+        Math.Sqrt(x) = int(Math.Sqrt(x))
+
+let res = isSquare(9)
+let res2 = isSquare(-3)
+let res3 = isSquare(5)
+
