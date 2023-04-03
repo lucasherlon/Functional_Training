20b064fdb878cb4bb5be5d50b4b499db3e9277dd
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2023-04-03 19:49:23 -0300

    exercism basics F#

---
+++ LucianLusciousLasagna.fs
@@ -0,0 +1,13 @@
+module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
+
+// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven elapsedTime = 40 - elapsedTime
+
+// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes layers = layers * 2
+
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes layers elapsedTime = (layers * 2) + elapsedTime
