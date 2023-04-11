1b9827e20778d244ab476dd01585c1498befdac7
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2023-04-11 18:34:50 -0300

    functions and guards in elixir

--- guessing_game.ex
+++ guessing_game.ex
@@ -4,6 +4,6 @@ defmodule GuessingGame do
   def compare(secret_number, guess) when guess - secret_number == 1, do: "So close"
   def compare(secret_number, guess) when secret_number - guess > 1, do: "Too low"
   def compare(secret_number, guess) when guess - secret_number > 1, do: "Too high"
-  def compare(secret_number), do: "Make a guess"
-  def compare(secret_number, :no_guess), do: "Make a guess"
+  def compare(_secret_number), do: "Make a guess"
+  def compare(_secret_number, :no_guess), do: "Make a guess"
 end
